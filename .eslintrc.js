94d19267b81e1e8e71ab37ebd56cdac8cd45a582
garrett-thompson <garrmt@gmail.com>
2021-11-21 17:47:31 -0500

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended'],
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log({ x: process.env.TEST });
