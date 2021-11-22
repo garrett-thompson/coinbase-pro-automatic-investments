62ab4dade2bfb246d9a4d762ed66f7966f05968d
garrett-thompson <garrmt@gmail.com>
2021-11-22 15:11:43 -0500

    add currency option

--- src/actions/check-available-balance/index.ts
+++ src/actions/check-available-balance/index.ts
@@ -3,7 +3,9 @@ import { request } from '../../utils/request';
 export async function checkAvailableBalance() {
   const { data: accounts } = await request({ requestMethod: 'GET', requestPath: '/accounts' });
 
-  const usdAccount = accounts.find((account: any) => account.currency === 'USD');
+  const defaultCurrency = process.env.CURRENCY || 'USD';
+
+  const usdAccount = accounts.find((account: any) => account.currency === defaultCurrency);
 
   const availableBalance = parseInt(usdAccount.available);
 
